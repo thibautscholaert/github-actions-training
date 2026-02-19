60dd5decdad0a008bce40c4b50edd9472cc6a3c3
Thibaut Scholaert <thibaut.scholaert@worldline.com>
2026-02-19 17:07:37 +0100

    feat: any

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
 
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo bounce" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -17,7 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count * 2)}>
           count is {count}
         </button>
         <p>
